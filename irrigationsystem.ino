da9f397eff1b3a4debb83266f009cd90a3708d01
Marco Mora <morafoo@gmail.com>
2022-02-24 21:43:42 -0600

    Initial commit

---
+++ irrigationsystem.ino
@@ -0,0 +1,18 @@
+/*----------------------------------------------------------------------*
+ *  _   __ _     _____ _____                                            *
+ * | | / /| |   |_   _/  __ \   This software is been developed by      *
+ * | |/ / | |     | | | /  \/   a group of enthusiast hobbiest          *
+ * |    \ | |     | | | |       with the purpose of learn and           *
+ * | |\  \| |_____| |_| \__/\   have fun, so nobody is responsible or   *
+ * \_| \_/\_____/\___/ \____/   will provide warranty.                  *
+ *                                                                      *
+ * This software will run in a ESP8266 microcontrolller, the objective  *
+ * is to have a irrigation system controler that can read some sensors  *
+ * and decide if a valve should be open.                                *
+ * There is not restriction to use, modify and improve the code, so     *
+ * please do it and share the improvements.                             *
+ *                                                                      *
+ * Letś have Fun!!                                                      *
+ *                                                                      *
+ * ---------------------------------------------------------------------*/
+
